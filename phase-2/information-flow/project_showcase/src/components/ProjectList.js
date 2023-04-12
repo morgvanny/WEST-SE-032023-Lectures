12a88e2be842437a367d8f231581a7dcd11af5c3
Morgan VanYperen <morgvanny@gmail.com>
2023-04-12 10:56:55 -0700

    init

--- phase-2/information-flow/project_showcase/src/components/ProjectList.js
+++ phase-2/information-flow/project_showcase/src/components/ProjectList.js
@@ -14,39 +14,39 @@ const ProjectList = ({ projects }) => {
 
   const handleOnChange = (e) => setSearchQuery(e.target.value);
 
-  // Deliverable 3: Refactor the filter component out of 
+  // Deliverable 3: Refactor the filter component out of
   // `ProjectList` and implement inverse data flow
 
-    // - Refactor the `searchQuery` state and the filter 
-    // method inside of the `ProjectList` component to 
-    // the `App` component
-  
-    // - Using inverse data flow, get the value of the 
-    // input field UP to the App component
-  
-    // - Write a callback function inside the App 
-    // component:
-  
-      //   - the function should take in a new search value 
-      // and set state with that value
-      
-      //   - pass the callback function down as a prop to 
-      // `ProjectList`
-  
-    // - Call the callback function from the onChange 
-    // event listener
+  // - Refactor the `searchQuery` state and the filter
+  // method inside of the `ProjectList` component to
+  // the `App` component
+
+  // - Using inverse data flow, get the value of the
+  // input field UP to the App component
+
+  // - Write a callback function inside the App
+  // component:
+
+  //   - the function should take in a new search value
+  // and set state with that value
+
+  //   - pass the callback function down as a prop to
+  // `ProjectList`
+
+  // - Call the callback function from the onChange
+  // event listener
 
   return (
     <section>
       <h2>Projects</h2>
 
       <div className="filter">
-        <button>All</button>
-        <button>Phase 5</button>
-        <button>Phase 4</button>
-        <button>Phase 3</button>
-        <button>Phase 2</button>
-        <button>Phase 1</button>
+        <button type="button">All</button>
+        <button type="button">Phase 5</button>
+        <button type="button">Phase 4</button>
+        <button type="button">Phase 3</button>
+        <button type="button">Phase 2</button>
+        <button type="button">Phase 1</button>
       </div>
       <input type="text" placeholder="Search..." onChange={handleOnChange} />
 
