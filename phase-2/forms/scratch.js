bb9afe5436282d8b2cf71f4ce1a5f085d6311893
Morgan VanYperen <morgvanny@gmail.com>
2023-04-13 09:57:21 -0700

    scratch

---
+++ phase-2/forms/scratch.js
@@ -0,0 +1,5 @@
+const dogs = [
+  { name: "Martin", age: 8, breed: "toy poodle/bichon frise" },
+  { name: "Maggie", age: 10, breed: "australian shepherd" },
+  { name: "Luna", age: 3, breed: "chi" },
+];
