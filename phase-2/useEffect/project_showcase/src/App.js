44754a4e957bab8cde078f9842e23ae94957e746
Morgan VanYperen <morgvanny@gmail.com>
2023-04-14 11:00:42 -0700

    init

--- phase-2/useEffect/project_showcase/src/App.js
+++ phase-2/useEffect/project_showcase/src/App.js
@@ -4,27 +4,6 @@ import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
 const App = () => {
-  // Deliverable 2: Implement useEffect in App component
-  // to load projects
-
-  // Import the `useEffect` hook from the React library
-
-  // Invoke `useEffect` and make a `GET` request using
-  // the `fetch` method
-
-  // Update `projects` state upon successful response
-  // from the server
-
-  // Deliverable 3: Demonstrate the order of operations
-  // between rendering a component and running the side
-  // effect
-
-  // Place a console.log() inside the `App` component as
-  // well as the `useEffect` method
-
-  // Open up the devtools to observe when each phase of
-  // the component will occur
-
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
