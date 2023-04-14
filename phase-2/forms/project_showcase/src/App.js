e0240c66839b3079019dcd95a8ac316242b8f244
Morgan VanYperen <morgvanny@gmail.com>
2023-04-14 08:06:25 -0700

    add link

--- phase-2/forms/project_showcase/src/App.js
+++ phase-2/forms/project_showcase/src/App.js
@@ -7,8 +7,21 @@ const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
+  const addNewProject = (project) => {
+    fetch("http://localhost:3000/projects", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(project),
+    })
+      .then((r) => r.json())
+      .then((p) => {
+        setProjects([...projects, p]);
+      });
+    // setProjects([...projects, project]);
+  };
+
   const handleClick = () => {
-    fetch("http://localhost:4000/projects")
+    fetch("http://localhost:3000/projects")
       .then((res) => res.json())
       .then((projects) => setProjects(projects));
   };
@@ -18,7 +31,7 @@ const App = () => {
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm />
+      <ProjectForm addNewProject={addNewProject} />
       <button onClick={handleClick}>Load Projects</button>
       <ProjectList projects={projects} />
     </div>
--- phase-2/forms/project_showcase/src/components/ProjectForm.js
+++ phase-2/forms/project_showcase/src/components/ProjectForm.js
@@ -1,17 +1,58 @@
-const ProjectForm = () => {
+import { useState } from "react";
+
+const ProjectForm = ({ addNewProject }) => {
+  const [newProject, setNewProject] = useState({
+    name: "",
+    about: "",
+    phase: "",
+    link: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    setNewProject({ ...newProject, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // update the projects state in App so the newProject is in that array
+    addNewProject(newProject);
+
+    setNewProject({
+      name: "",
+      about: "",
+      phase: "",
+      link: "",
+      image: "",
+    });
+  };
+
+  const { name, about, phase, link, image } = newProject;
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form onSubmit={handleSubmit} className="form" autoComplete="off">
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          value={name}
+          onChange={handleChange}
+          type="text"
+          id="name"
+          name="name"
+        />
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea
+          value={about}
+          onChange={handleChange}
+          id="about"
+          name="about"
+        />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select value={phase} onChange={handleChange} name="phase" id="phase">
           <option>Select One</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
@@ -21,10 +62,22 @@ const ProjectForm = () => {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input
+          value={link}
+          onChange={handleChange}
+          type="text"
+          id="link"
+          name="link"
+        />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input
+          value={image}
+          onChange={handleChange}
+          type="text"
+          id="image"
+          name="image"
+        />
 
         <button type="submit">Add Project</button>
       </form>
