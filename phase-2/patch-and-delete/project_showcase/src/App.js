c355685206be7bd07fe67b4f434f39912a653c5c
Morgan VanYperen <morgvanny@gmail.com>
2023-04-17 10:46:38 -0700

    init

---
+++ phase-2/patch-and-delete/project_showcase/src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+import ProjectEditForm from "./components/ProjectEditForm";
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [projects, setProjects] = useState([]);
+  const [projectId, setProjectId] = useState(null);
+
+  const selectedProject = projects.find((p) => p.id === projectId);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/projects")
+      .then((resp) => resp.json())
+      .then((projects) => setProjects(projects));
+  }, []);
+
+  const onToggleDarkMode = () => {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  };
+
+  const onAddProject = (newProj) => {
+    setProjects((projects) => [...projects, newProj]);
+  };
+
+  const completeEditing = () => {
+    setProjectId(null);
+  };
+
+  const enterProjectEditModeFor = (projectId) => {
+    setProjectId(projectId);
+  };
+
+  const renderForm = projectId ? (
+    <ProjectEditForm
+      key={projectId}
+      project={selectedProject}
+      completeEditing={completeEditing}
+    />
+  ) : (
+    <ProjectForm onAddProject={onAddProject} />
+  );
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      {renderForm}
+      <ProjectList
+        projects={projects}
+        enterProjectEditModeFor={enterProjectEditModeFor}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ phase-2/patch-and-delete/project_showcase/src/components/ProjectEditForm.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const ProjectEditForm = ({ project, completeEditing }) => {
+  const [formData, setFormData] = useState(project);
+
+  const { name, about, phase, link, image } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Add code here
+    completeEditing();
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form" autoComplete="off">
+      <h3>Edit Project</h3>
+
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="about">About</label>
+      <textarea id="about" name="about" value={about} onChange={handleChange} />
+
+      <label htmlFor="phase">Phase</label>
+      <select name="phase" id="phase" value={phase} onChange={handleChange}>
+        <option value="1">Phase 1</option>
+        <option value="2">Phase 2</option>
+        <option value="3">Phase 3</option>
+        <option value="4">Phase 4</option>
+        <option value="5">Phase 5</option>
+      </select>
+
+      <label htmlFor="link">Project Homepage</label>
+      <input
+        type="text"
+        id="link"
+        name="link"
+        value={link}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="image">Screenshot</label>
+      <input
+        type="text"
+        id="image"
+        name="image"
+        value={image}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Update Project</button>
+    </form>
+  );
+};
+
+export default ProjectEditForm;
