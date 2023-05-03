625e065ca6d9b1c47feb241eb1004d5404d4a0ac
Morgan VanYperen <morgvanny@gmail.com>
2023-05-03 12:10:49 -0700

    done

--- phase-3/object_oriented_python/debug.py
+++ phase-3/object_oriented_python/debug.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+import ipdb
 from lib.pet import *
 
 
-import ipdb; ipdb.set_trace()
+some_pet = Pet("rose", 2, "husky", "tired", "3209.jpg")
+another_pet = Pet("jim", 6, 'poodle', 'excited', '131.jpg')
+
+some_pet.print_details()
+
+
+ipdb.set_trace()
--- phase-3/object_oriented_python/lib/pet.py
+++ phase-3/object_oriented_python/lib/pet.py
@@ -1,50 +1,61 @@
 #!/usr/bin/env python3
-# Demonstrate classes 
+# Demonstrate classes
 # 1. ✅ Create a Pet class
-# 2. ✅ Instantiate a few pet instance 
-    # Compare the pet instances to demonstrate they are not the same object
-    # Note: add 'pass' to the pet class 
-
+# 2. ✅ Instantiate a few pet instance
+# Compare the pet instances to demonstrate they are not the same object
+# Note: add 'pass' to the pet class
 
 class Pet:
-    # pass
-# 3. ✅ Demonstrate __init__ 
-    # Add arguments to instances  
-    # use dot notation to access their attributes 
-    # update attributes with new values 
+    def __init__(self, name, age, breed, temperament, image_url):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.temperament = temperament
+        self.image_url = image_url
 
+    def print_details(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Breed: {self.breed}")
+        print(f"Temperament: {self.temperament}")
+        print(f"Image Url: {self.image_url}")
 
-# 4.✅ Demonstrate instance methods by creating a print_pet_details function that will print the pet attributes
-#     Review the self keyword 
-#     Invoke the print_pet_details on an instance 
-#           -> 
-            # name:rose
-            # age:11
-            # breed:domestic longhair
-            # temperament:sweet
-            # image_url:rose.jpg
+    def one_more(self, arg):
+        print(f"{self.name} is {arg}")
+        # 3. ✅ Demonstrate __init__
+        # Add arguments to instances
+        # use dot notation to access their attributes
+        # update attributes with new values
 
+        # 4.✅ Demonstrate instance methods by creating a print_pet_details function that will print the pet attributes
+        #     Review the self keyword
+        #     Invoke the print_pet_details on an instance
+        #           ->
+        # name:rose
+        # age:11
+        # breed:domestic longhair
+        # temperament:sweet
+        # image_url:rose.jpg
 
-# Demonstrate instances 
-    # Different Instances are Different Objects
-# Demonstrate __init__
-# Demonstrate instance method
-# Demonstrate the self keyword 
-# Stretch Goals
-# Demonstrate object properties
+        # Demonstrate instances
+        # Different Instances are Different Objects
+        # Demonstrate __init__
+        # Demonstrate instance method
+        # Demonstrate the self keyword
+        # Stretch Goals
+        # Demonstrate object properties
 
-# Instances 
+        # Instances
 
-# Run in ipdb session
-# rose == cookie
-#   False
+        # Run in ipdb session
+        # rose == cookie
+        #   False
 
-#Read Attributes 
-# rose.name -> rose
-# rose.age -> 11
+        # Read Attributes
+        # rose.name -> rose
+        # rose.age -> 11
 
-#Update
-# rose.age -> 11
-# rose.age = 12
-# rose.age -> 12
-
+        # Update
+        # rose.age -> 11
+        # rose.age = 12
+        # rose.age -> 12
