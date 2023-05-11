f49b73d8e5941290655339e43c2d236b2be47ca2
Morgan VanYperen <morgvanny@gmail.com>
2023-05-11 13:16:32 -0700

    init

--- phase-3/orm/lib/debug.py
+++ phase-3/orm/lib/debug.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
 
+import ipdb
 from owner import Owner, CONN, CURSOR
 from pet import Pet, CONN, CURSOR
-
-sql = """
-    DROP TABLE IF EXISTS pets
-    DROP TABLE IF EXISTS owner
-"""
-
-CURSOR.execute(sql)
 
 # Owner.create_table()
 # frank = Owner("frank", "555-555-5555", "frank@gmail.com", "555 Somewhere St.")
 # frank.save()
 
-Pet.create_table()
-spot = Pet("spot", "dog", "chihuahua", "feisty", 1)
-spot.save()
+# Pet.create_table()
+# spot = Pet("spot", "dog", "chihuahua", "feisty", 1)
+# spot.save()
 
-import ipdb; ipdb.set_trace()
+ipdb.set_trace()
