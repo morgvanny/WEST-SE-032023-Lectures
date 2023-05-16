d6cc1b33bb516088f3a41832b00a0059b4c1b0a5
Morgan VanYperen <morgvanny@gmail.com>
2023-05-16 09:20:05 -0700

    done

--- phase-3/SQLAlchemy_and_alembic/app/debug.py
+++ phase-3/SQLAlchemy_and_alembic/app/debug.py
@@ -7,48 +7,71 @@ from sqlalchemy.orm import sessionmaker
 from models import (Base, Pet)
 
 if __name__ == '__main__':
-    #3.1 ✅ Uncomment bellow to create the engine
-    # engine = create_engine('sqlite:///pet_app.db')
-    # Base.metadata.create_all(engine)
-    #3.2 ✅ Uncomment bellow to create sessions and bind o the engine
-    # Session = sessionmaker(bind=engine)
-    # session = Session()
+    # 3.1 ✅ Uncomment bellow to create the engine
+    engine = create_engine('sqlite:///pet_app.db')
+    Base.metadata.create_all(engine)
+    # 3.2 ✅ Uncomment bellow to create sessions and bind o the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    #3.3 ✅  -- Creating records
-        # Create a pet and save it to the data base with session.add and session.commit
+    # 3.3 ✅  -- Creating records
+    # Create a pet and save it to the data base with session.add and session.commit
+    # rose = Pet(name="Rose", species="dog", breed="poodle", temperament="happy")
+    # # Create multiple pets and bulk save them with  session.bulk_save_objects and session.commit
+    # golden_retriever_names = ["Bob", "Jane", "Mary", "Bill"]
+    # golden_retrievers = [Pet(name=name, species="dog", breed="golden retriever", temperament="sad")
+    #                      for name in golden_retriever_names]
 
-        # Create multiple pets and bulk save them with  session.bulk_save_objects and session.commit
- 
+    # # session.bulk_save_objects(golden_retrievers)
+    # # session.commit()
 
-        #session.add(rose)
-        #Note: bulk save will not contain the id
+    pets = [pet for pet in session.query(Pet)]
 
-        #verify by checking the db 
-    #3.4 ✅ Read
-        # Get all with session.query
-        # Print the pets 
+    pet_names = [name for name in session.query(Pet.name)]
 
-        #Get all of the pet names and print them with session.query
-   
-        #Get all the pet names and print them in order with session.query and order_by
+    sad_pets = [pet for pet in session.query(
+        Pet).filter(Pet.temperament.like('%sad%'))]
 
-        #Get the first pet with session.query and first
+    first_pet = session.query(Pet).first()
 
+    # session.delete(first_pet)
+    # session.commit()
 
-        #Filter pet by temperament with session.query and filter 
+    # session.query(Pet).delete()
+    # session.commit()
 
+    # first_pet.name = "Original Rose"
 
-    #3.5 ✅ Update 
-        # Update the pets name and print the updated pet info
-  
-        # Update all the pets temperament to 'cool' and print the pets 
-   
+    # session.query(Pet).update({Pet.temperament: "Excited"})
 
-    #3.6 ✅  Delete
-        # Delete one item by querying the first pet, deleting it and committing it
+    # session.commit()
 
-        #delete all the pets with session.query and .delete
-  
+    # session.add(rose)
+    # Note: bulk save will not contain the id
+
+    # verify by checking the db
+    # 3.4 ✅ Read
+    # Get all with session.query
+    # Print the pets
+
+    # Get all of the pet names and print them with session.query
+
+    # Get all the pet names and print them in order with session.query and order_by
+
+    # Get the first pet with session.query and first
+
+    # Filter pet by temperament with session.query and filter
+
+    # 3.5 ✅ Update
+    # Update the pets name and print the updated pet info
+
+    # Update all the pets temperament to 'cool' and print the pets
+
+    # 3.6 ✅  Delete
+    # Delete one item by querying the first pet, deleting it and committing it
+
+    # delete all the pets with session.query and .delete
 
     # optional Break point for debugging and testing
-    import ipdb; ipdb.set_trace()
+    import ipdb
+    ipdb.set_trace()
