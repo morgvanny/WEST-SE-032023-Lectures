d58a281e0a0cded2ff861e5659afeaf6e89e8de0
Morgan VanYperen <morgvanny@gmail.com>
2023-06-05 11:00:28 -0700

    add auth

---
+++ phase-4/auth/server/app.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# 📚 Review With Students:
+# Set up:
+    # cd into server and run the following in Terminal:
+        # export FLASK_APP=app.py
+        # export FLASK_RUN_PORT=5000
+        # flask db init
+        # flask db revision --autogenerate -m'Create tables' 
+        # flask db upgrade 
+        # python seed.py
+# Running React Together 
+     # In Terminal, run:
+        # `honcho start -f Procfile.dev`
+
+from flask import Flask, request, make_response, session, jsonify, abort
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound, Unauthorized
+from flask_cors import CORS
+
+# 1.✅ Import Bcrypt form flask_bcrypt
+    #1.1 Invoke Bcrypt and pass it app
+from flask_bcrypt import Bcrypt
+
+
+# 2.✅ Navigate to "models.py"
+    # Continue on Step 3
+
+app = Flask(__name__)
+CORS(app) 
+bcrypt = Bcrypt(app)
+from models import db, Production, CastMember, User
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+app.secret_key = b'@~xH\xf2\x10k\x07hp\x85\xa6N\xde\xd4\xcd'
+
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Productions(Resource):
+    def get(self):
+        production_list = [p.to_dict() for p in Production.query.all()]
+        response = make_response(
+            production_list,
+            200,
+        )
+
+        return response
+
+    def post(self):
+        form_json = request.get_json()
+        try:
+            new_production = Production(
+                title=form_json['title'],
+                genre=form_json['genre'],
+                budget=int(form_json['budget']),
+                image=form_json['image'],
+                director=form_json['director'],
+                description=form_json['description']
+            )
+        except ValueError as e:
+            abort(422,e.args[0])
+
+        db.session.add(new_production)
+        db.session.commit()
+
+        response_dict = new_production.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+        return response
+api.add_resource(Productions, '/productions')
+
+
+class ProductionByID(Resource):
+    def get(self,id):
+        production = Production.query.filter_by(id=id).first()
+        if not production:
+            raise NotFound
+        production_dict = production.to_dict()
+        response = make_response(
+            production_dict,
+            200
+        )
+        
+        return response
+
+    def patch(self, id):
+        production = Production.query.filter_by(id=id).first()
+        if not production:
+            raise NotFound
+
+        for attr in request.form:
+            setattr(production, attr, request.form[attr])
+
+        production.ongoing = bool(request.form['ongoing'])
+        production.budget = int(request.form['budget'])
+
+        db.session.add(production)
+        db.session.commit()
+
+        production_dict = production.to_dict()
+        
+        response = make_response(
+            production_dict,
+            200
+        )
+        return response
+
+    def delete(self, id):
+        production = Production.query.filter_by(id=id).first()
+        if not production:
+            raise NotFound
+        db.session.delete(production)
+        db.session.commit()
+
+        response = make_response('', 204)
+        
+        return response
+api.add_resource(ProductionByID, '/productions/<int:id>')
+
+# 14.✅ Create a Signup route
+class Signup(Resource):
+    def post(self):
+        form_json = request.get_json()
+        new_user = User(name=form_json['name'], email=form_json['email'])
+        #Hashes our password and saves it to _password_hash
+        new_user.password_hash = form_json['password']
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        response = make_response(
+            new_user.to_dict(),
+            201
+        )
+        return response
+api.add_resource(Signup, '/signup')
+
+# User.query.order_by(User.id.desc()).first()._password_hash
+
+# 15.✅ Create a Login route
+class Login(Resource):
+    def post(self):
+        try:
+            user = User.query.filter_by(name=request.get_json()['name']).first()
+            if user.authenticate(request.get_json()['password']):
+                session['user_id'] = user.id
+                response = make_response(
+                    user.to_dict(),
+                    200
+                )
+                return response
+        except:
+            abort(401, "Incorrect Username or Password")
+
+api.add_resource(Login, '/login')
+# 16.✅ Create a route that checks to see if the User is currently in sessions
+    # 16.1 Use add_resource to add an authorized endpoint
+    # 16.2 Create a Get method
+        #16.2.1 Check to see if the user_id is in session
+        #16.2.2 If found query the user and send it to the client
+        #16.2.3 If not found return a 401 Unauthorized error
+class AuthorizedSession(Resource):
+    def get(self):
+        try:
+            user = User.query.filter_by(id=session['user_id']).first()
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+            return response
+        except:
+            abort(401, "Unauthorized")
+
+api.add_resource(AuthorizedSession, '/authorized')
+
+# 17.✅ Create a Logout route
+    #17.1 Use add_resource to add a logout endpoint
+    #17.2 Create a delete method
+        # 8.2.1 Set the user_id in sessions to None
+        # 8.2.1 Create a response with no content and a 204
+    #17.3 Test out your route with the client or Postman
+
+# 18.✅ Navigate to client navigation
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None 
+        response = make_response('',204)
+        return response
+api.add_resource(Logout, '/logout')
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    response = make_response(
+        "Not Found: Sorry the resource you are looking for does not exist",
+        404
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ phase-4/auth/server/models.py
@@ -0,0 +1,100 @@
+# 📚 Review With Students:
+   # The dangers of plain text passwords 
+   # What Hashing is
+     # Hashing vs encryption 
+     # How to Hash a string   
+   # Salting 
+     #Rainbow Tables
+   # Bcrypt
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+
+# 3.✅ Import bcyrpt from app
+from app import bcrypt
+
+
+db = SQLAlchemy()
+class Production(db.Model, SerializerMixin):
+    __tablename__ = 'productions'
+    
+    __table_args__ = (
+        db.CheckConstraint('budget > 100'),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+      
+    title = db.Column(db.String, nullable=False)
+    genre = db.Column(db.String, nullable=False)
+    budget = db.Column(db.Float)
+    image = db.Column(db.String)
+    director = db.Column(db.String)
+    description = db.Column(db.String)
+    ongoing = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    cast_members = db.relationship('CastMember', backref='production')
+        
+    serialize_rules = ('-cast_members.production',)
+
+    @validates('image')
+    def validate_image(self, key, image_path):
+        if '.jpg' not in image_path:
+            raise ValueError("Image file type must be a jpg")
+        return image_path
+    
+
+    def __repr__(self):
+        return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
+
+class CastMember(db.Model, SerializerMixin):
+    __tablename__ = 'cast_members'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    role = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
+    
+    serialize_rules = ('-production.cast_members',)
+
+    def __repr__(self):
+        return f'<Production Name:{self.name}, Role:{self.role}'
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+
+    # 4.✅ Add a column _password_hash
+    _password_hash = db.Column(db.String)
+    admin = db.Column(db.String, default=False)
+
+   
+    # 5.✅ Create a hybrid_property 
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+  
+    # 11.✅ Create a setter method called password_hash that takes self and a password.
+        #11.1 Use bcyrpt to generate the password hash with bcrypt.generate_password_hash
+        #11.2 Set the _password_hash to the hashed password
+    # user.password_hash('cow')
+    # lsib#482#^**2nkjdfhgh
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+     # 12.✅ Create an authenticate method that uses bcyrpt to verify the password against the hash in the DB with bcrypt.check_password_hash 
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+     # 13.✅ Navigate to app
+    def __repr__(self):
+        return f'USER: ID: {self.id}, Name {self.name}, Email: {self.email}, Admin: {self.admin}'
