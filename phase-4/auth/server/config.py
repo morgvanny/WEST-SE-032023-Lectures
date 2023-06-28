4dd129d301c7438836abf166dfa3240d74660347
Morgan VanYperen <morgvanny@gmail.com>
2023-06-28 09:24:20 -0700

    done

--- phase-4/auth/server/config.py
+++ phase-4/auth/server/config.py
@@ -3,9 +3,12 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+load_dotenv('.env')
+app.secret_key = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
